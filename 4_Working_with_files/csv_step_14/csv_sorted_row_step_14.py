6d384fa0f503442e3481950f68ccb6c263eda211
evgenij <jenkz1@yandex.ru>
2023-04-16 00:04:30 +0300

    [init] Итоговое решение задачи

--- 4_Working_with_files/csv_step_14/csv_sorted_row_step_14.py
+++ 4_Working_with_files/csv_step_14/csv_sorted_row_step_14.py
@@ -1,6 +1,13 @@
-import requests
 import csv
 
+
 with open(file='deniro.csv', mode='r', encoding='utf-8') as deniro:
-    print(deniro.read())
+    data = csv.reader(deniro)
+
+    input_user_row = int(input()) - 1
+
+    sort_iterable = sorted(data, key=lambda row: int(row[input_user_row]) if row[input_user_row].isdigit() else row[input_user_row])
+
+    for rows in sort_iterable:
+        print(','.join(rows))
 
