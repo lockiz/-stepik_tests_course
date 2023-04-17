d026ceaf06045ccb8203495adf0817f31fd5d6bd
evgenij <jenkz1@yandex.ru>
2023-04-17 23:08:42 +0300

    Готовое решение задачи

--- 4_Working_with_files/json_step_8_merge_object/main.py
+++ 4_Working_with_files/json_step_8_merge_object/main.py
@@ -1,3 +1,9 @@
 import json
 
 
+with open('data1.json', 'r', encoding='utf-8') as file_1, open('data2.json', 'r', encoding='utf-8') as file_2:
+    data_1, data_2 = json.load(file_1), json.load(file_2)
+    data_1.update(data_2)
+
+with open('data_merge.json', mode='w', encoding='utf-8') as save_file:
+    json.dump(data_1, save_file, indent='   ')
