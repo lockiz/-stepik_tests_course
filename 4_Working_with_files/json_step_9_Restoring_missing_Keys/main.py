304ed2ca7dd4695e8fbcfb6b228e8499eefab643
evgenij <jenkz1@yandex.ru>
2023-04-17 23:39:52 +0300

    Правильное решение задачи с использованием функции max и setdefault

--- 4_Working_with_files/json_step_9_Restoring_missing_Keys/main.py
+++ 4_Working_with_files/json_step_9_Restoring_missing_Keys/main.py
@@ -2,3 +2,13 @@ import json
 
 with open('people.json', mode='r', encoding='utf-8') as file:
     data = json.load(file)
+    keys = list(max(data, key=lambda x: len(x)).keys())
+    temp = []
+    for row in data:
+        for i in keys:
+            row.setdefault(i, None)
+        temp.append(row)
+
+with open('updated_people.json', mode='w', encoding='utf-8') as save_file:
+    json.dump(temp, save_file, indent='   ', sort_keys=True)
+
