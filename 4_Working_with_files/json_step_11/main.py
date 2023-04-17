b234e79b84342bff1dffba0d3aba4e05f03958b9
evgenij <jenkz1@yandex.ru>
2023-04-18 00:28:28 +0300

    Правильное решение задачи)

---
+++ 4_Working_with_files/json_step_11/main.py
@@ -0,0 +1,10 @@
+import csv
+import json
+
+with open('playgrounds.csv', mode='r', encoding='utf-8') as file:
+    data_csv, temp = csv.DictReader(file, delimiter=';'), {}
+    for row in data_csv:
+        temp.setdefault(row['AdmArea'], {}).setdefault(row['District'], []).append(row['Address'])
+
+with open('addresses.json', mode='w', encoding='utf-8') as save_file:
+    json.dump(temp, save_file, indent='   ', ensure_ascii=False)
