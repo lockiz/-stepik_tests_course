5dc52801088b8afc554982a91160779d889223a7
evgenij <jenkz1@yandex.ru>
2023-04-18 17:53:19 +0300

    Правильное решение задачи

--- 4_Working_with_files/json_step_14_🌶/main.py
+++ 4_Working_with_files/json_step_14_🌶/main.py
@@ -3,23 +3,15 @@ import csv
 from datetime import datetime
 
 with open('exam_results.csv', mode='r', encoding='UTF-8') as csv_file:
-    data = sorted(list(csv.DictReader(csv_file)), key=lambda x: datetime.strptime(x['date_and_time'], '%Y-%m-%d \
+    rows = sorted(list(csv.DictReader(csv_file)), key=lambda x: datetime.strptime(x['date_and_time'], '%Y-%m-%d \
     %H:%M:%S'))
 
     temp = {}
-    for row in data:
-        if temp.get(row['email'], 0) == 0:
-            temp[row['email']] = {
-                'name': row['name'],
-                'surname': row['surname'],
-                'best_score': int(row['score']),
-                'date_and_time': row['date_and_time'],
-                'email': row['email']
-            }
-        else:
-            if int(temp[row['email']]['best_score']) <= int(row['score']):
-                temp[row['email']]['best_score'] = int(row['score'])
-                temp[row['email']]['date_and_time'] = row['date_and_time']
+    for row in rows:
+        print(row['email'])
+        print(sorted(rows, key=lambda x: x['email'] == row['email']))
+        break
 
-with open('best_scores.json', mode='w', encoding='UTF-8') as save_file:
-    json.dump(sorted(list(temp.values()), key=lambda x: x['email']), save_file, indent=3)
+
+# with open('best_scores.json', mode='w', encoding='UTF-8') as save_file:
+#     json.dump(sorted(list(temp.values()), key=lambda x: x['email']), save_file, indent=3)
