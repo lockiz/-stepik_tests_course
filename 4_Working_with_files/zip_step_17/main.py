05b4f18b846e787369bcbaf15baa50ce00568c86
evgenij <jenkz1@yandex.ru>
2023-04-19 14:29:36 +0300

    Правильное решение задачи

--- 4_Working_with_files/zip_step_17/main.py
+++ 4_Working_with_files/zip_step_17/main.py
@@ -1,3 +1,9 @@
 from zipfile import ZipFile
+from datetime import datetime
 
-with ZipFile('', mode='r') as zip_file:
+
+with ZipFile('workbook.zip', mode='r') as zip_file:
+    t = datetime.strptime('2021-11-30 14:22:00', '%Y-%m-%d %H:%M:%S')
+    for file in sorted(list(zip_file.infolist()), key=lambda x: x.filename.split('/')[-1]):
+        if (not file.is_dir()) and (datetime(*file.date_time) >= t):
+            print(file.filename.split('/')[-1])
