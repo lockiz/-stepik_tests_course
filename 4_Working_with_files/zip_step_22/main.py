ed81472a3c6321e5a7303341cf43200019d9d6b3
evgenij <jenkz1@yandex.ru>
2023-04-20 00:05:58 +0300

    Правильное решение задачи

--- 4_Working_with_files/zip_step_22/main.py
+++ 4_Working_with_files/zip_step_22/main.py
@@ -1,9 +1,21 @@
+from zipfile import ZipFile
 import json
 
 
-def is_correct_json(string):
+def is_correct_json(file_json):
     try:
-        json.loads(string)
-        return True
-    except json.decoder.JSONDecodeError:
+        return json.load(file_json)
+    except (json.decoder.JSONDecodeError, UnicodeDecodeError):
         return False
+
+
+with ZipFile('data.zip', mode='r') as zip_file:
+    result = []
+    for name_file in zip_file.infolist():
+        if name_file.filename.split('.')[-1] == 'json':
+            with zip_file.open(name_file, mode='r') as file:
+                data = is_correct_json(file)
+                if data and data['team'] == 'Arsenal':
+                    result.append(f"{data['first_name']} {data['last_name']}")
+
+    print(*sorted(result), sep='\n')
