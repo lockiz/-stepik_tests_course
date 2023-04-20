8be09594207b1798675b1fbe07a621a3098a623d
evgenij <jenkz1@yandex.ru>
2023-04-21 01:25:18 +0300

    Готовое решение на сегодня, я спать)

---
+++ 4_Working_with_files/pickle_step_15/main.py
@@ -0,0 +1,19 @@
+import pickle
+
+file_name, check_sum = input(), int(input())
+answer = ('Контрольные суммы не совпадают', 'Контрольные суммы совпадают')
+
+with open(file_name, mode='rb') as file_pkl:
+    data = pickle.load(file_pkl)
+    nums = [i for i in data if isinstance(i, int)]
+
+    if isinstance(data, dict):
+        print(answer[sum(nums) == check_sum])
+    else:
+        print(answer[(min(nums) * max(nums) if any(nums) else 0) == check_sum])
+
+
+nums = []
+print(min(nums) * max(nums) if any(nums) else 0)
+
+print(any([]))
